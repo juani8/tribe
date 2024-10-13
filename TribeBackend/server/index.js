0c170845d56ec0bb6213c7fa39b92b59dfc9d046
Rosario Presedo <mpresedo@uade.edu.ar>
2024-10-13 15:22:02 -0300

    Conexión a base de datos en la nube Mongo DB Atlas

--- TribeBackend/server/index.js
+++ TribeBackend/server/index.js
@@ -1,9 +1,25 @@
-const express = require("express");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+const connection = require("./db");
+const app = require("./app");
 
-const app = express();
+// Establecer conexión a la base de datos
+connection();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+/**
+ * Función que inicializa la aplicación Express y la hace escuchar en un puerto específico.
+ * Lee el puerto del entorno o utiliza el puerto 8080 por defecto.
+ * 
+ * @returns {Promise<void>} Retorna una promesa vacía.
+ */
+async function init() {
+  // Obtener el puerto del entorno o usar 8080 como puerto por defecto
+  const port = process.env.PORT || 8080;
+  // Iniciar la aplicación Express y hacerla escuchar en el puerto especificado
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
+
+// Iniciar la aplicación
+init();
