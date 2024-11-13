6d4b11b2ea4af01e135a8856af9ce0daa960e1d8
Rosario Presedo <mpresedo@uade.edu.ar>
2024-11-12 21:58:03 -0300

    Update index.js

--- TribeBackend/server/index.js
+++ TribeBackend/server/index.js
@@ -8,14 +8,16 @@ async function init() {
     await connection();
     const port = process.env.PORT || 8080;
     const host = process.env.HOST || 'http://localhost';
-    app.listen(port, () => {
-      // Si el host de Azure esta definido, Azure establece automáticamente el puerto para la aplicación.
-      if (process.env.HOST) {
+    
+    if (process.env.HOST) {
+      app.listen(() => {
         console.log(`Servidor escuchando en: ${host}`);
-      } else {
+      });
+    } else {
+      app.listen(port, () => {
         console.log(`Servidor escuchando en: ${host}:${port}`);
-      }
-    });
+      });
+    }
   } catch (error) {
     console.error("Error al iniciar la aplicación:", error);
     process.exit(1);
