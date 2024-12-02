0b4063e0d62ac7a2080fdf3dbefc46cd3c66e900
candezeida <candelazeida@gmail.com>
2024-12-02 03:07:11 -0300

    Modified user model and implemented google sign in functionality

--- TribeBackend/routes/authRoutes.js
+++ TribeBackend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { sendTotp, verifyTotp, register, login, requestPasswordReset, resetPasswordWithToken, validateToken } = require('../controllers/authController');
+const { sendTotp, verifyTotp, register, login, googleLogin, requestPasswordReset, resetPasswordWithToken, validateToken } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/send-totp', sendTotp);
 router.post('/verify-totp', verifyTotp);
 router.post('/registrations', register);
 router.post('/sessions', login);
+router.post('/sessions/google-login', googleLogin);
 router.post('/sessions/passwords', requestPasswordReset);
 router.patch('/sessions/passwords', resetPasswordWithToken);
 router.post('/validate-token', validateToken);
