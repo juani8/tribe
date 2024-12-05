3fa4c1618e3564e480e0078a44399484163f9820
candezeida <candelazeida@gmail.com>
2024-12-04 23:16:34 -0300

    added recover password validations

--- TribeBackend/models/User.js
+++ TribeBackend/models/User.js
@@ -58,7 +58,8 @@ const userSchema = new mongoose.Schema({
     followers: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-    }]
+    }],
+    resetTokenUsed: { type: Boolean, default: false },
 }, { timestamps: true });
 
 // Hook de pre-guardado para validar la contraseña en función de isGoogleUser.
